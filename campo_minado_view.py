476a1a04703c6cb17c81b479fad8a25cd7a42466
Jeferson Araujo de Sousa <jefersonaraujo95@gmail.com>
2017-09-25 14:20:34 -0300

    Melhorado a restauração do jogo monolitico

--- campo_minado_view.py
+++ campo_minado_view.py
@@ -1,38 +1,54 @@
 from campo_minado_negocio import CampoMinado
+from os.path import isfile
+from os import remove
+import json
 
-objeto = CampoMinado(3, 5)
-
-
-#objeto.menu()
-
-print("#########################################")
-print("#              CAMPO MINADO             #")
-print("#########################################")
-print("#1 - INICIAR JOGO                       #")
-print("#2 - SAIR                               #")
-print("#########################################\n")
-
-op = int(input("Inserir Opção :"))
+objeto = CampoMinado(2, 2)
 
 
 
-while objeto.proxima_jogada():
-    #print (objeto.proxima_jogada())
+
+def menu():
+    print("#########################################")
+    print("#              CAMPO MINADO             #")
+    print("#########################################")
+    print("#1 - INICIAR JOGO                       #")
+    print("#2 - RESTAURAR                          #")
+    print("#3 - SAIR                               #")
+    print("#########################################\n")
+    opcao = int(input("Inserir Opção :"))
+    if opcao == 1:
+        start()
+    elif opcao == 2:
+        partida()
+    else:
+        pass
+
+""" FIM MENU"""
+
+def start ():
+    if objeto.proxima_jogada():
+        objeto.imprimir_tabuleiro()
+        linha = int(input("Posição da linha :"))
+        coluna = int(input("Posição da coluna :"))
+        objeto.jogada(linha,coluna)
+        start()
+    else:
+        print("Fim")
 
 
-    objeto.imprimir_tabuleiro()
+def partida():
+    if isfile("game.json"):
+        arquivo = open("game.json")
+        game = json.loads(arquivo.read())
+        objeto.restaurar(game)
+        arquivo.close()
+        start()
+    else:
+        print("Não há jogo salvo !\n")
+        
 
-    linha = int(input("Entre com posicao da linha :"))
-    coluna = int(input("Entre com posicao da coluna :")) #input("Entre com posicao da coluna :")
-
-    objeto.jogada(linha,coluna)
 
 
-objeto.imprimir_tabuleiro()
 
-""" 1. Menu para iniciar o jogo
-    2. Menu declara jogada
-    3. Regra pra vitoria
-
-    4. Salvar jogadas
-    5.Continuar jogo"""
+menu()
